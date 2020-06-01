036f9daaf34ccd9a8008782684c5724f13495fa7
Faouzi mohamed <faouzimohamedf@gmail.com>
2020-06-01 07:03:19 +0100

    Update Js

--- js/body.js
+++ js/body.js
@@ -1,22 +1,49 @@
-var ul = document.querySelector("#ul-aside-nav")
-var section = document.querySelectorAll("section")
-var node = ''
+function new_element(name,attributes={}, text=''){
+    node = document.createElement(name);
 
-for(var i = 0,c=section.length;i<c;i++)
-{
-    childs = section[i]
-    for(var j=0, l=childs.childNodes.length;j < l;j++)
-    {
-        node = childs.childNodes[j]
+    for(var o in attributes)
+        node.setAttribute(o, attributes[o])
 
-        if(node.nodeName === 'H2')
-            alert(node.nodeName + " : " +
-                node.innerHTML)
-        else if(node.nodeName === 'H3')
-            alert(node.nodeName + " : " +
-                node.innerHTML)
-
+    if(text){
+        node.innerHTML = text;
     }
-        
+    return node
 }
-//if(node = section[0])
+
+function create_list_left_nav(){
+    var ul = document.querySelector("#ul-aside-nav")
+    var section = document.querySelectorAll("section")
+    var node = '', a = '',  li = '',content=''
+    
+    for(var i = 0,c=section.length;i<c;i++)
+    {
+        childs = section[i]
+        for(var j=0, l=childs.childNodes.length;j < l;j++)
+        {
+            node = childs.childNodes[j]
+            id = 'titre'+i
+
+                
+            if(node.nodeName === 'H2')
+            {
+                node.id += id
+                a  = new_element('a', {href:'#'+id},node.firstChild.data)
+                li = new_element('li')
+                li.appendChild(a)
+                ul.appendChild(li)
+            }
+            else if(node.nodeName === 'H3')
+            {
+
+            }
+        }
+            
+    }
+
+
+    console.log(li.innerHTML)
+
+
+}
+
+create_list_left_nav()
