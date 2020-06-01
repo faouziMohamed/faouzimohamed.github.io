0e8b96fde56fbf6fb7b9b6bb48fb5431cdb702ed
Faouzi mohamed <faouzimohamedf@gmail.com>
2020-06-01 03:34:43 +0100

    Update tree

--- js/nav.js
+++ js/nav.js
@@ -1,29 +1,7 @@
-// function to set a given theme/color-scheme
-function setTheme(themeName)
-{
-    localStorage.setItem('theme', themeName);
-    document.documentElement.className = themeName;
-}
-// function to toggle between light and dark theme
-function toggleTheme()
-{
-    if (localStorage.getItem('theme') === 'theme-dark')
-    {setTheme('theme-light');} else 
-    { setTheme('theme-dark');}
-}
-// Immediately invoked function to set the theme on initial load
-(function () 
-{
-    if (localStorage.getItem('theme') === 'theme-dark')
-    { setTheme('theme-dark'); } else
-    { setTheme('theme-light'); }
-})();
-
 
 var open = false
 var close = true
-function openMenu()
-{
+function openMenu(){
     var li = document.querySelectorAll("ul#main-list>li");
 
     for (var i=1, c=li.length; i<c;i++){
@@ -42,13 +20,11 @@ function openMenu()
     close = false
 }
 
-function closeMenu()
-{
+function closeMenu(){
     var li = document.querySelectorAll("ul#main-list>li:first-child~li");
     for (var i=0, c=li.length; i<c;i++){
         li[i].setAttribute("style","disp lay:none");
     }
-
     open  = false
     close = true    
 }  
