45ee1c0dd789c3571e3afabf7fad3c7f6e093448
Faouzi mohamed <faouzimohamedf@gmail.com>
2020-06-04 00:24:04 +0100

    Commit

--- js/nav.js
+++ js/nav.js
@@ -3,51 +3,51 @@ var open = false;
 var close = true;
 var used = false;
 
-function openMenu(){
+function openMenu() {
     var ul = document.querySelector("ul#main-list");
     ul.style.display = 'block';
-    
+
     var section = document.querySelectorAll(".main-section");
-    for (var i=0, c=section.length; i < c;++i){
-        section[i].setAttribute("onmouseover","closeMenu()");
-        section[i].setAttribute("onclick","closeMenu()");
+    for (var i = 0, c = section.length; i < c; ++i) {
+        section[i].setAttribute("onmouseover", "closeMenu()");
+        section[i].setAttribute("onclick", "closeMenu()");
     }
-    open  = true;
+    open = true;
     close = false;
 }
 
-function closeMenu(){
+function closeMenu() {
     var ul = document.querySelector("ul#main-list");
     ul.style.display = 'none';
 
-    open  = false;
-    close = true;    
-}  
+    open = false;
+    close = true;
+}
 
-function open_close_Menu(){
+function open_close_Menu() {
     used = true;
     var x = window.matchMedia("(max-width: 536px)");
-    if(x.matches){
-        if(open) closeMenu();
-        else     openMenu();
+    if (x.matches) {
+        if (open) closeMenu();
+        else openMenu();
     }
     x.addListener(open_close_Menu);
-    
+
 }
 
 
-(function(){
+(function () {
     var ul = document.querySelector("ul#main-list");
-    function display_menu(x){
-        if(x.matches){
+
+    function display_menu(x) {
+        if (x.matches) {
             ul.style.display = 'block';
             var section = document.querySelectorAll(".main-section");
-            for (var i=0, c=section.length; i < c;++i){
-                section[i].setAttribute("onmouseover","");
-                section[i].setAttribute("onclick","");
+            for (var i = 0, c = section.length; i < c; ++i) {
+                section[i].setAttribute("onmouseover", "");
+                section[i].setAttribute("onclick", "");
             }
-        }
-        else {
+        } else {
             ul.style.display = 'none';
         }
         alerte("oui")
@@ -59,21 +59,19 @@ function open_close_Menu(){
 
 
 (function () {
-    function tune_submenu(y)
-    {
+    function tune_submenu(y) {
         var ul = document.querySelectorAll(".submenu");
         var parent;
-        if(y.matches)
-        {
-            
-            for (var i=0, c=ul.length;i < c;++i){
+        if (y.matches) {
+
+            for (var i = 0, c = ul.length; i < c; ++i) {
                 parent = ul[i].parentElement;
                 parent.className += ' subMenuParent'
             }
-        }else{
-            for (var i=0, c=ul.length;i < c;++i){
+        } else {
+            for (var i = 0, c = ul.length; i < c; ++i) {
                 parent = ul[i].parentElement;
-                parent.className.replace(' subMenuParent','')
+                parent.className.replace(' subMenuParent', '')
             }
         }
     }
