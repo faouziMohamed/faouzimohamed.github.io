41a98b9b2f6e504107de0f005689997a6f519ac5
Faouzi mohamed <faouzimohamedf@gmail.com>
2020-06-03 02:30:30 +0100

    Update

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,12 @@
 
 var open = false
 var close = true
+var used = false
 function openMenu(){
     var ul = document.querySelector("ul#main-list");
     ul.style.display = 'block'
     
-    var section = document.querySelectorAll(".main-section>*")
+    var section = document.querySelectorAll(".main-section")
     for (var i=0, c=section.length; i < c;++i){
         section[i].setAttribute("onmouseover","closeMenu()")
         section[i].setAttribute("onclick","closeMenu()")
@@ -23,6 +24,30 @@ function closeMenu(){
 }  
 
 function open_close_Menu(){
-    if(open) closeMenu()
-    else     openMenu()
+    used = true
+    var x = window.matchMedia("(max-width: 536px)")
+    if(x.matches){
+        if(open) closeMenu()
+        else     openMenu()
+    }
+    x.addListener(open_close_Menu)
+    
 }
+
+
+var ul = document.querySelectorAll(".submenu")
+
+for (var i=0, c=ul.length;i < c;++i){
+    var parent = ul[i].parentElement
+    parent.style.position = 'relative'
+}
+
+var funct = (function (x){
+    if(x.matches){
+        alert(used)
+    }
+})();
+
+var x = window.matchMedia("(min-width: 538px)")
+
+    x.addListener(funct)
