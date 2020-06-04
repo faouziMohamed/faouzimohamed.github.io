f394f9570c11a2d17c6a216b94bffd7c4efbfac7
Faouzi mohamed <faouzimohamedf@gmail.com>
2020-06-04 02:48:09 +0100

    Update Js

--- js/nav.js
+++ js/nav.js
@@ -9,8 +9,8 @@ function openMenu() {
 
     var section = document.querySelectorAll(".main-section");
     for (var i = 0, c = section.length; i < c; ++i) {
-        section[i].setAttribute("onmouseover", "closeMenu()");
-        section[i].setAttribute("onclick", "closeMenu()");
+        section[i].addEventListener('mouseover', closeMenu);
+        section[i].addEventListener("click", closeMenu);
     }
     open = true;
     close = false;
@@ -44,15 +44,12 @@ function open_close_Menu() {
             ul.style.display = 'block';
             var section = document.querySelectorAll(".main-section");
             for (var i = 0, c = section.length; i < c; ++i) {
-                section[i].setAttribute("onmouseover", "");
-                section[i].setAttribute("onclick", "");
+                section[i].removeEventListener('mouseover', closeMenu);
+                section[i].removeEventListener("click", closeMenu);
             }
-
         } else {
             ul.style.display = 'none';
-
         }
-
     }
     var x = window.matchMedia("(min-width: 538px)");
     display_menu(x);
@@ -61,20 +58,30 @@ function open_close_Menu() {
 
 
 (function () {
+    var ul = document.querySelectorAll(".submenu");
+    var parent;
+    var c = ul.length
+    var display;
+    for (var i = 0; i < c; i++) {
+        parent = ul[i].parentElement;
+        parent.addEventListener('click',function (e){
+            if(this.lastElementChild.style.display !== 'block')
+                this.lastElementChild.style.display = 'block'
+            else
+                this.lastElementChild.style.display = 'none'
+        });
+    }
+
     function tune_submenu(y) {
-        var ul = document.querySelectorAll(".submenu");
-        var parent;
         if (y.matches) {
-
-            for (var i = 0, c = ul.length; i < c; ++i) {
+            for (var i = 0; i < c; ++i) {
                 parent = ul[i].parentElement;
-                parent.className += ' subMenuParent'
+                parent.classList.add('subMenuParent');
             }
-
         } else {
-            for (var i = 0, c = ul.length; i < c; ++i) {
+            for (var i = 0; i < c; ++i) {
                 parent = ul[i].parentElement;
-                parent.classList.remove('subMenuParent')
+                parent.classList.remove('subMenuParent');
             }
         }
     }
@@ -82,40 +89,11 @@ function open_close_Menu() {
     var y = window.matchMedia("(max-width: 536px)");
     tune_submenu(y);
     y.addListener(tune_submenu);
+
+    
 })();
 
-
-
-
-
-
-
-
 /*
-(
-function () {
-        function tune_submenu(y)
-        {
-            if(y.matches)
-            {
-                var ul = document.querySelectorAll(".submenu");
-                var parent;
-                for (var i=0, c=ul.length;i < c;++i){
-                    parent = ul[i].parentElement;
-                    parent.style.position = 'relative';
-                }
-            }else{
-                var ul = document.querySelectorAll(".submenu");
-                var parent;
-                for (var i=0, c=ul.length;i < c;++i){
-                    parent = ul[i].parentElement;
-                }
-            }
-        }
-
-        var y = window.matchMedia("(max-width: 536px)");
-        tune_submenu(y);
-        y.addListener(display_menu);
-    }
-)();
-*/
+(function (){
+    
+})();*/
