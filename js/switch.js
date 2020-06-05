472470923dad288933de2ac5528e277eca0e4fe1
Faouzi mohamed <faouzimohamedf@gmail.com>
2020-06-05 06:48:31 +0100

    Toogle theme fixed

--- js/switch.js
+++ js/switch.js
@@ -10,12 +10,21 @@ function toggleTheme()
     if (localStorage.getItem('theme') === 'theme-dark')
     {setTheme('theme-light');} else 
     { setTheme('theme-dark');}
+    //alert(local Storage.getItem('theme'))
 }
 // Immediately invoked function to set the theme on initial load
 (function () 
 {
+    var slider = document.querySelector("#slider")
     if (localStorage.getItem('theme') === 'theme-dark')
-    { setTheme('theme-dark'); } else
-    { setTheme('theme-light'); }
-    setTheme('theme-light');
+    { 
+        setTheme('theme-dark'); 
+        slider.checked = true;
+    } else
+    { 
+        setTheme('theme-light');
+        slider.checked = false;
+    }
 })();
+
+//localStorage.clear()
