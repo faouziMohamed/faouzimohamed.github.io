c889052a18607dc602cf619f01086e23c6dfcfe8
Faouzi mohamed <faouzimohamedf@gmail.com>
2020-06-05 06:50:49 +0100

    Some add

--- js/switch.js
+++ js/switch.js
@@ -1,29 +1,26 @@
 // function to set a given theme/color-scheme
-function setTheme(themeName)
-{
+function setTheme(themeName) {
     localStorage.setItem('theme', themeName);
     document.documentElement.className = themeName;
 }
 // function to toggle between light and dark theme
-function toggleTheme()
-{
-    if (localStorage.getItem('theme') === 'theme-dark')
-    {setTheme('theme-light');} else 
-    { setTheme('theme-dark');}
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-light');
+    } else {
+        setTheme('theme-dark');
+    }
     //alert(local Storage.getItem('theme'))
 }
 // Immediately invoked function to set the theme on initial load
-(function () 
-{
+(function () {
     var slider = document.querySelector("#slider")
-    if (localStorage.getItem('theme') === 'theme-dark')
-    { 
-        setTheme('theme-dark'); 
-        slider.checked = true;
-    } else
-    { 
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-dark');
+        if (slider) slider.checked = true;
+    } else {
         setTheme('theme-light');
-        slider.checked = false;
+        if (slider) slider.checked = false;
     }
 })();
 
