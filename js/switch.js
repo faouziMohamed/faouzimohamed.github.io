02b8ea07b25a8ba01909817e35447b332d48fcd9
Faouzi mohamed <faouzimohamedf@gmail.com>
2020-06-05 05:56:34 +0100

    Update

--- js/switch.js
+++ js/switch.js
@@ -1,17 +1,21 @@
 // function to set a given theme/color-scheme
-function setTheme(themeName){
+function setTheme(themeName)
+{
     localStorage.setItem('theme', themeName);
     document.documentElement.className = themeName;
 }
 // function to toggle between light and dark theme
-function toggleTheme(){
+function toggleTheme()
+{
     if (localStorage.getItem('theme') === 'theme-dark')
     {setTheme('theme-light');} else 
     { setTheme('theme-dark');}
 }
 // Immediately invoked function to set the theme on initial load
-(function (){
+(function () 
+{
     if (localStorage.getItem('theme') === 'theme-dark')
     { setTheme('theme-dark'); } else
     { setTheme('theme-light'); }
+    setTheme('theme-light');
 })();
