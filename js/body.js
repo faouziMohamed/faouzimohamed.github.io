a0779cb7a085109d976f75642239bf23156aedcc
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2020-06-10 03:14:34 +0100

    Update

--- js/body.js
+++ js/body.js
@@ -17,7 +17,7 @@ function insertAfter(el, ref) {
 
 function add_li_to_ul(ul,node,id)
 {
-    node.id += id;
+    node.id = id;
     a  = new_element('a', {href:'#'+id},node.firstChild.data);
     var li = new_element('li');
     li.appendChild(a);
@@ -31,26 +31,28 @@ function create_list_left_nav()
     var ul = document.querySelector("#ul-aside-nav");
     var title = document.querySelectorAll("section h2, section h3");
     var node = null,  n=1, id=null, li=null;
-    var H1 = document.querySelector(".main-article H1")
+    
+    var H1 = document.querySelector(".main-article H1");
+    H1.id = "top-h1";
+
     var link_in_h2 = new_element('a',{style:"text-decoration:none;\
                                               color:lightgreen;",
-                                      href:"#top-h1"},
+                                      href:"#top-h1 "},
                         document.querySelector(".main-article H1")
                                 .firstChild.data) ;
     var h2 = new_element('h2',{style:'text-align:left;\
                                        font-size:110%;\
                                        margin-left:0px'});
     h2.appendChild(link_in_h2);
-    ul.parentNode
-    .insertBefore(h2,ul);
-    H1.id += " top-h1";
+    ul.parentNode.insertBefore(h2,ul);
+    
     for(var i=0, c=title.length; i<c;++i)
     {
-        id  = ' titre'+ (n++);
+        id  = 'titre'+ (n++);
         li  = add_li_to_ul(ul,title[i],id);
         ul_ = new_element('ul');
         while(++i<c && title[i].nodeName === 'H3'){
-            id  = ' titre'+ (n++);
+            id  = 'titre'+ (n++);
             add_li_to_ul(ul_,title[i],id);
         }
         li.appendChild(ul_);
