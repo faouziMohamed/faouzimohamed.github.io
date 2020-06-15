b7fa0e10dd603769f8304b4981c9755a48f51948
Faouzi mohamed <faouzimohamedf@gmail.com>
2020-06-15 04:21:26 +0100

    Update Css and js rendering

--- js/body.js
+++ js/body.js
@@ -1,10 +1,10 @@
-function new_element(name,attributes={}, text=''){
+function new_element(name, attributes = {}, text = '') {
     node = document.createElement(name);
 
-    for(var o in attributes)
+    for (var o in attributes)
         node.setAttribute(o, attributes[o]);
 
-    if(text){
+    if (text) {
         node.innerHTML = text;
     }
     return node;
@@ -12,13 +12,14 @@ function new_element(name,attributes={}, text=''){
 
 function insertAfter(el, ref) {
     ref.parentNode
-       .insertBefore(el, ref.nextSibling);
+        .insertBefore(el, ref.nextSibling);
 }
 
-function add_li_to_ul(ul,node,id)
-{
+function add_li_to_ul(ul, node, id) {
     node.id = id;
-    a  = new_element('a', {href:'#'+id},node.firstChild.data);
+    a = new_element('a', {
+        href: '#' + id
+    }, node.firstChild.data);
     var li = new_element('li');
     li.appendChild(a);
     ul.appendChild(li);
@@ -26,62 +27,74 @@ function add_li_to_ul(ul,node,id)
 }
 
 
-void function create_list_left_nav()
-{
+void
+function create_list_left_nav() {
     var ul = document.querySelector("#ul-aside-nav");
     var title = document.querySelectorAll("section h2, section h3, section h4");
-    var node = null,  n=1, id=null, li=null;
-    var ul_=null,li_=null,_ul=null, c=title.length;
+    var node = null,
+        n = 1,
+        id = null,
+        li = null;
+    var ul_ = null,
+        li_ = null,
+        _ul = null,
+        c = title.length;
     /*For H1*/
     var H1 = document.querySelector(".main-article H1");
     H1.id = "top-h1";
 
-    var h2 = new_element('h2',{style:'text-align:left; font-size:110%; margin-left:0px'});
-    h2.appendChild(new_element('a',{style:"text-decoration:none; color:lightgreen;", href:"#top-h1"},
-                                document.querySelector(".main-article H1").firstChild.data));
-    ul.parentNode.insertBefore(h2,ul);
-    
+    var h2 = new_element('h2', {
+        style: 'text-align:left; font-size:110%; margin-left:0px'
+    });
+    h2.appendChild(new_element('a', {
+            style: "text-decoration:none; color:lightgreen;",
+            href: "#top-h1"
+        },
+        document.querySelector(".main-article H1").firstChild.data));
+    ul.parentNode.insertBefore(h2, ul);
+
     /*Main loop to create the list of title in the left side of the webpage*/
-    for(var i=0; i<c;++i)
-    {   /*For H2*/
-        if(title[i].nodeName === 'H2'){
-            li  = add_li_to_ul(ul,title[i++],'titre'+ (n++));
+    for (var i = 0; i < c; ++i) 
+    {
+        /*For H2*/
+        if (title[i].nodeName === 'H2') {
+            li = add_li_to_ul(ul, title[i++], 'titre' + (n++));
             ul_ = new_element('ul');
         }
         /*For H3*/
-        while(i<c && (title[i].nodeName === 'H3')){
-            li_ = add_li_to_ul(ul_,title[i++],'titre'+ (n++));
+        while (i < c && (title[i].nodeName === 'H3')) {
+            li_ = add_li_to_ul(ul_, title[i++], 'titre' + (n++));
         }
         /*For H4*/
-        if(i<c && title[i].nodeName === 'H4'){
+        if (i < c && title[i].nodeName === 'H4') {
             _ul = new_element('ul');
-            while(i<c && title[i].nodeName === 'H4'){
-                add_li_to_ul(_ul,title[i++],'titre'+ (n++));
+            while (i < c && title[i].nodeName === 'H4') {
+                add_li_to_ul(_ul, title[i++], 'titre' + (n++));
             }
             li_.appendChild(_ul);
             --i;
-        }
-        else i--;
+        } else i--;
         li.appendChild(ul_);
     }
 }();
 
 
-void function click_on_aside_nav_Link(){
-    var link = document.querySelectorAll("a[href*='#titre']");
+void
+function click_on_internal_link_CSS_transition() {
+    var link = document.querySelectorAll("main a[href*='#']:not(#top)");
     var nav = document.querySelector("#header-nav");
     var c = link.length;
 
-    for(var i=0; i<c; ++i){
-        link[i].addEventListener('click',function (){
+    for (var i = 0; i < c; ++i) {
+        link[i].addEventListener('click', function () {
             nav.style.visibility = 'hidden';
             nav.style.opacity = 0;
         })
     }
 
-    document.body.addEventListener('wheel',function (){
+    document.body.addEventListener('wheel', function () {
         var nodeStyle = nav.style;
-        if(nodeStyle.visibility==='hidden'){
+        if (nodeStyle.visibility === 'hidden') {
             nodeStyle.visibility = 'visible';
             nodeStyle.opacity = 1;
         }
