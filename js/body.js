601810c11e5d449bbb69d8668551592a6b3cd51e
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2020-06-16 05:49:33 +0100

    simple quote  swapped by double qoute on every string

--- js/body.js
+++ js/body.js
@@ -1,4 +1,4 @@
-function new_element(name, attributes = {}, text = '') {
+function new_element(name, attributes = {}, text = "") {
     node = document.createElement(name);
 
     for (var o in attributes)
@@ -17,10 +17,10 @@ function insertAfter(el, ref) {
 
 function add_li_to_ul(ul, node, id) {
     node.id = id;
-    a = new_element('a', {
-        href: '#' + id
+    a = new_element("a", {
+        href: "#" + id
     }, node.firstChild.data);
-    var li = new_element('li');
+    var li = new_element("li");
     li.appendChild(a);
     ul.appendChild(li);
     return li;
@@ -43,10 +43,10 @@ function create_list_left_nav() {
     var H1 = document.querySelector(".main-article H1");
     H1.id = "top-h1";
 
-    var h2 = new_element('h2', {
-        style: 'text-align:left; font-size:110%; margin-left:0px'
+    var h2 = new_element("h2", {
+        style: "text-align:left; font-size:110%; margin-left:0px"
     });
-    h2.appendChild(new_element('a', {
+    h2.appendChild(new_element("a", {
             style: "text-decoration:none; color:lightgreen;",
             href: "#top-h1"
         },
@@ -56,19 +56,19 @@ function create_list_left_nav() {
     /*Main loop to create the list of title in the left side of the webpage*/
     for (var i = 0; i < c; ++i) {
         /*For H2*/
-        if (title[i].nodeName === 'H2') {
-            li = add_li_to_ul(ul, title[i++], 'titre' + (n++));
-            ul_ = new_element('ul');
+        if (title[i].nodeName === "H2") {
+            li = add_li_to_ul(ul, title[i++], "titre" + (n++));
+            ul_ = new_element("ul");
         }
         /*For H3*/
-        while (i < c && (title[i].nodeName === 'H3')) {
-            li_ = add_li_to_ul(ul_, title[i++], 'titre' + (n++));
+        while (i < c && (title[i].nodeName === "H3")) {
+            li_ = add_li_to_ul(ul_, title[i++], "titre" + (n++));
         }
         /*For H4*/
-        if (i < c && title[i].nodeName === 'H4') {
-            _ul = new_element('ul');
-            while (i < c && title[i].nodeName === 'H4') {
-                add_li_to_ul(_ul, title[i++], 'titre' + (n++));
+        if (i < c && title[i].nodeName === "H4") {
+            _ul = new_element("ul");
+            while (i < c && title[i].nodeName === "H4") {
+                add_li_to_ul(_ul, title[i++], "titre" + (n++));
             }
             li_.appendChild(_ul);
             --i;
@@ -86,20 +86,20 @@ function click_on_internal_link_CSS_transition() {
     var c = link.length;
 
     for (var i = 0; i < c; ++i) {
-        link[i].addEventListener('click', function () {
-            nav.style.visibility = 'hidden';
+        link[i].addEventListener("click", function () {
+            nav.style.visibility = "hidden";
             nav.style.opacity = 0;
         })
     }
 
     function make_visible() {
         var nodeStyle = nav.style;
-        if (nodeStyle.visibility === 'hidden') {
-            nodeStyle.visibility = 'visible';
+        if (nodeStyle.visibility === "hidden") {
+            nodeStyle.visibility = "visible";
             nodeStyle.opacity = 1;
         }
     }
 
-    document.body.addEventListener('touchstart', make_visible, true); //For tactil 
-    document.body.addEventListener('wheel', make_visible); //for touchpad or mouse wheel
+    document.body.addEventListener("touchstart", make_visible, true); //For tactil 
+    document.body.addEventListener("wheel", make_visible); //for touchpad or mouse wheel
 }();
