54a23818a30255d50355704ad0defc5c49eddd6f
Faouzi mohamed <faouzimohamedf@gmail.com>
2020-06-20 07:38:11 +0100

    modal block added

--- js/modal-things.js
+++ js/modal-things.js
@@ -1,6 +1,7 @@
 function newTxtNode(text) {
     return document.createTextNode(text);
 }
+
 function newElement(name, attributes = {}, text = "") {
     let node = document.createElement(name);
     const keys = Object.getOwnPropertyNames(attributes);
@@ -14,28 +15,38 @@ function newElement(name, attributes = {}, text = "") {
     return node;
 }
 
-function modalContent(img){
-    let modal        = newElement("section",{class: "modal"});
-    let modalContent = newElement("div",{class:"modal-content"});
-    let imgClone     = img.cloneNode(true);
-    let close        = newElement("span",{class:"close"},"&times;");
-
-    console.log(imgClone);
+function modalContent(img) {
+    let modal = newElement("section", {
+        class: "modal",
+        style: "display:block"
+    });
+    let modalContent = newElement("div", {
+        class: "modal-content"
+    });
+    let imgClone = img.cloneNode(true);
+    let close = newElement("i", {
+        class: "fas fa-close close"
+    });
+    close.addEventListener("click", () => {
+        document.body.removeChild(modal);
+    })
     modalContent.appendChild(close);
     modalContent.appendChild(imgClone);
     modal.appendChild(modalContent);
-    return modalContent;
+    return modal;
 }
 
 
 let fig = document.querySelectorAll("figure img, figure svg");
-let c=fig.length;
+let c = fig.length;
 
-function modalWin(e){
+function newModalBox(e) {
     let modal = modalContent(this);
-    this.appendChild(modal);
+    console.log(modal);
+
+    document.body.appendChild(modal);
 }
 
-for(let ii=0; ii<c; ++ii){
-    fig[ii].addEventListener('click', modalWin);
+for (let ii = 0; ii < c; ++ii) {
+    fig[ii].addEventListener('click', newModalBox);
 }
