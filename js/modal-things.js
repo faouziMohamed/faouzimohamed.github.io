3282f91e64d2dc124494d7183b964635f8bacfdb
Faouzi mohamed <faouzimohamedf@gmail.com>
2020-06-22 00:24:39 +0100

    modal-things resolved

--- js/modal-things.js
+++ js/modal-things.js
@@ -25,30 +25,22 @@ function modalContent(img) {
     });
     let imgClone = img.cloneNode(true);
     //imgClone.setAttribute("data-theme","switch");
-    console.log(imgClone);
     let close = newElement("i", {
         class: "fas fa-close close"
     });
     close.addEventListener("click", () => {
         document.body.removeChild(modal);
-    })
+    });
     modalContent.appendChild(close);
     modalContent.appendChild(imgClone);
     modal.appendChild(modalContent);
     return modal;
 }
 
-
-void  (function displayModalContent()
-{
-    let fig = document.querySelectorAll("figure img, figure svg");
-    let c = fig.length;
-
-    for (let ii = 0; ii < c; ++ii) 
-    {
-        fig[ii].addEventListener('click', function ()
-        {
+void(function displayModalContent() {
+    document.querySelectorAll("figure img, figure svg").forEach((img) => {
+        img.addEventListener("click", function () {
             document.body.appendChild(modalContent(this));
         });
-    }
+    });
 }());
