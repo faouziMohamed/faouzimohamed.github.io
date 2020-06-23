5416c0ed4e8ad7aefe0b4349d226a6ee93124f54
Faouzi mohamed <faouzimohamedf@gmail.com>
2020-06-23 03:54:20 +0100

    switching img on slideshow ready

--- js/img-slide.js
+++ js/img-slide.js
@@ -1,10 +1,24 @@
-let slideIndex = 1;
-/*
-function displaySlide(n, node="") {
-    let i;
-    let slides = document.querySelectorAll(".slide");
-    let dots = document.querySelectorAll(".dot");
-    //let slider=document.querySelectorAll(".slider-container");
+function newTxtNode(text) {
+    return document.createTextNode(text);
+}
+
+function newElement(name, attributes = {}, text = "") {
+    let node = document.createElement(name);
+    const keys = Object.getOwnPropertyNames(attributes);
+    keys.forEach((key) => {
+        node.setAttribute(`${key}`, attributes[`${key}`]);
+    });
+
+    if (text) {
+        node.appendChild(newTxtNode(text));
+    }
+    return node;
+}
+
+function displaySlide(n, slideShowParent) {
+    let slides = slideShowParent.querySelectorAll(".slide");
+    let dots = slideShowParent.querySelectorAll(".dot");
+    let slideIndex = localStorage.getItem(`slideIndex_${slideShowParent.dataset.id}`);
 
     if (!slides.length) {
         return;
@@ -16,41 +30,59 @@ function displaySlide(n, node="") {
     if (n < 1) {
         slideIndex = slides.length;
     }
+
     slides.forEach((node) => {
         node.style.display = "none";
     });
     dots.forEach((dot) => {
         dot.classList.remove("active");
     });
+
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].classList.toggle("active");
-    localStorage.setItem("slideIndex", slideIndex);
-}
-*/
-function changeImg(n) {
-    //displaySlide(slideIndex += n);
+    localStorage.setItem(`slideIndex_${slideShowParent.dataset.id}`, slideIndex);
 }
 
-function currentSlide(n) {
-    //displaySlide(slideIndex = n);
+function changeImg(n, slideShowParent) {
+    let currentIndex = localStorage.getItem(`slideIndex_${slideShowParent.dataset.id}`);
+    localStorage.setItem(`slideIndex_${slideShowParent.dataset.id}`, currentIndex + n);
+    displaySlide(n, slideShowParent);
 }
 
-function configureSlideShow() {
-    let slideIndex;
+function currentSlide(n, parent) {
+    localStorage.setItem(`slideIndex_${parent.parentNode.dataset.id}`, n);
+    displaySlide(n, parent.parentNode);
+}
+
+
+function sliderDots(n) {
+    let dotsParent = newElement("div", {
+        class: "dot-wrapper"
+    });
+
+    for (let i = 1; i <= n; ++i) {
+        dotsParent.appendChild(newElement("span", {
+            class: "dot",
+            onclick: `currentSlide(${i},this.parentNode)`
+        }));
+    }
+
+    return dotsParent;
+}
+
+void(function configureSlideShow() {
+    let i = 0;
     document.querySelectorAll("[data-id*='slide_']").forEach((slideShowParent) => {
-        //console.log(newElement);
-        slideIndex = Number(localStorage.getItem(`slideIndex_${slideShowParent.dataset.id}`));
+        let figures = slideShowParent.querySelectorAll("figure");
+        let slideIndex = localStorage.getItem(`slideIndex_${slideShowParent.dataset.id}`);
+        slideShowParent.appendChild(sliderDots(figures.length));
         if (slideIndex) {
-            //displaySlide(slideIndex, slideShowParent);
+            if(i===2) slideIndex=1;
+            displaySlide(slideIndex, slideShowParent);
         } else {
             slideIndex = 1;
             localStorage.setItem(`slideIndex_${slideShowParent.dataset.id}`, 1);
-            //displaySlide(slideIndex, slideShowParent);
+            displaySlide(slideIndex, slideShowParent);
         }
     });
-}
-
-
-export {
-    configureSlideShow
-};
+}());
--- js/script.js
+++ js/script.js
@@ -10,72 +10,31 @@ import {
 } from "./body.js";
 
 import {
+    newElement,
     executeMainNavMenu,
 } from "./nav.js";
 
-import {
-    configureSlideShow
-} from "./img-slide.js";
+function include(fileName, isModule = false) {
+    let prefix, type;
+    if (document.title !== "La classification automatique") {
+        prefix = "../js/";
+    } else {
+        prefix = "js/";
+    }
+
+    (isModule == true) ? type = "module": type = "text/javascript";
+
+    document.body.appendChild(newElement("script", {
+        src: `${prefix}${fileName}`,
+        type: `${type}`,
+        defer: "true",
+    }));
+}
+
 void(function main() {
     setThemeAfterPageLoaded();
     displayModalContent();
     executeMainNavMenu();
     executeBody();
-    configureSlideShow();
+    include("img-slide.js",false);
 }());
-
-/*function newTxtNode(text) {
-    return document.createTextNode(text);
-}
-
-function newElement(name, attributes = {}, text = "") {
-    let node = document.createElement(name);
-    const keys = Object.getOwnPropertyNames(attributes);
-    keys.forEach((key) => {
-        node.setAttribute(`${key}`, attributes[`${key}`]);
-    });
-
-    if (text) {
-        node.appendChild(newTxtNode(text));
-    }
-    return node;
-}
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function include(fileName) {
-    let prefix;
-    if(document.title!=="La classification automatique"){
-        prefix = "../js/";
-    }
-    else{
-        prefix = "js/";
-    }
-    document.head.appendChild(newElement("script", {
-        src: `${prefix}${fileName}`,
-        type: 'text/javascript',
-        defer: 'true',
-        //async:true
-    }));
-    console.log(this.document.title);
-}
-*/
